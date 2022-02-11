157fb6e85fd3aae14e82bf7784d7736c31065865
gllmp <guillaumepiccarreta@gmail.com>
2022-02-11 12:22:44 +0100

    init

---
+++ main.js
@@ -0,0 +1,75 @@
+import 'ol/ol.css';
+import './assets/css/style.css';
+import Map from 'ol/Map';
+import Overlay from 'ol/Overlay';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import XYZ from 'ol/source/XYZ';
+import {toLonLat} from 'ol/proj';
+import {toStringHDMS} from 'ol/coordinate';
+
+/**
+ * Elements that make up the popup.
+ */
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+/**
+ * Create an overlay to anchor the popup to the map.
+ */
+const overlay = new Overlay({
+  element: container,
+  autoPan: {
+    animation: {
+      duration: 250,
+    },
+  },
+});
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+ */
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
+const key = 'inKLFdB7xglu2Az8uVYx';
+const attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+/**
+ * Create the map.
+ */
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        attributions: attributions,
+        url: 'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=' + key,
+        tileSize: 512,
+      }),
+    }),
+  ],
+  overlays: [overlay],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function (evt) {
+  const coordinate = evt.coordinate;
+  const hdms = toStringHDMS(toLonLat(coordinate));
+
+  content.innerHTML = '<p>You clicked here:</p><code>' + hdms + '</code>';
+  overlay.setPosition(coordinate);
+});
